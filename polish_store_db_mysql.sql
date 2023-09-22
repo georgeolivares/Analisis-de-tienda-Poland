80428695b9a3a80753480d9216210c493839dc90
georgeolivares <88570786+georgeolivares@users.noreply.github.com>
2023-09-21 18:53:45 -0600

    Finished creating products table

--- polish_store_db_mysql.sql
+++ polish_store_db_mysql.sql
@@ -15,13 +15,52 @@ SELECT COUNT(*) AS 'RecordCount' FROM polish_store.monthly_sales_by_product;
 SELECT * FROM polish_store.monthly_sales_by_product LIMIT 10;
 
 -- LOCATE UNIQUE PRODUCT VALUES FROM SALES TABLE
-SELECT DISTINCT polish_store.monthly_sales_by_product.Product_Id_Num FROM polish_store.monthly_sales_by_product;
+SELECT DISTINCT polish_store.monthly_sales_by_product.Product_Id_Num FROM polish_store.monthly_sales_by_product ORDER BY polish_store.monthly_sales_by_product.Product_Id_Num;
 
-SELECT polish_store.monthly_sales_by_product.Product_Id_Num, 
-polish_store.monthly_sales_by_product.Product_name, polish_store.monthly_sales_by_product.Product_group
-FROM polish_store.monthly_sales_by_product
-WHERE polish_store.monthly_sales_by_product.Product_Id_Num
-IN (SELECT DISTINCT polish_store.monthly_sales_by_product.Product_Id_Num FROM polish_store.monthly_sales_by_product);
+
+-- VERIFY WE DO NOT HAVE ANY PRODUCT IDs WITH DIFFERENT PRODUCT NAMES IN THE MONTHLY SALES DATA.
+-- IT SEEMS LIKE WE DO NOT HAVE ANY WRONG PRODUCT NAMES.
+SELECT 
+	polish_store.monthly_sales_by_product.Product_Id_Num, COUNT(DISTINCT polish_store.monthly_sales_by_product.Product_name) AS UniqueProductNames
+FROM
+	polish_store.monthly_sales_by_product
+GROUP BY 
+	polish_store.monthly_sales_by_product.Product_Id_Num
+HAVING UniqueProductNames > 1;
+
+
+-- CREATE TEMP TABLE. 
+-- I SELECTED THE BASIC COLUMNS, I WILL NOT ADD COLUMNS SUCH AS AVG PURCHASE AND SALE PRICE AS THEY CHANGE CONSTANTLY.
+
+SELECT 
+	ps.Product_Id_Num,
+    MAX(ps.Product_name) AS ProductName,
+    MAX(ps.Product_group) AS ProductGroup
+FROM
+	polish_store.monthly_sales_by_product AS ps
+GROUP BY
+	ps.Product_Id_Num
+ORDER BY
+	ps.Product_Id_Num;
+
+
+-- CREATE TABKE OF PRODUCTS
+CREATE TABLE Products AS
+SELECT 
+	ps.Product_Id_Num,
+    MAX(ps.Product_name) AS ProductName,
+    MAX(ps.Product_group) AS ProductGroup
+FROM
+	polish_store.monthly_sales_by_product AS ps
+GROUP BY
+	ps.Product_Id_Num
+ORDER BY
+	ps.Product_Id_Num;
+
+
+
+-- VERIFY NEW TABLE CREATION
+SELECT * FROM polish_store.products;
 
 
 
