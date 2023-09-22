5597001fb3a8581cf05b6051f0a2ac2955a95c3c
georgeolivares <88570786+georgeolivares@users.noreply.github.com>
2023-09-21 20:05:45 -0600

    primary keys

--- polish_store_db_mysql.sql
+++ polish_store_db_mysql.sql
@@ -1,18 +1,23 @@
 -- USE DATABASE
 USE polish_store;
 
+
 -- VERIFY DATA IS CORRECT
 SELECT * FROM daily_sales;
+
 
 -- UPLOADED MONTHLY SALES FILE FROM DBEAVER
 ALTER TABLE polish_store.newmonthlysalesbyproduct
 RENAME TO polish_store.monthly_sales_by_product;
 
+
 -- VERIFY THAT ALL RECORDS WERE UPLOADED TO THE TABLE
 SELECT COUNT(*) AS 'RecordCount' FROM polish_store.monthly_sales_by_product;
 
+
 -- CREATE PRODUCTS TABLE
 SELECT * FROM polish_store.monthly_sales_by_product LIMIT 10;
+
 
 -- LOCATE UNIQUE PRODUCT VALUES FROM SALES TABLE
 SELECT DISTINCT polish_store.monthly_sales_by_product.Product_Id_Num FROM polish_store.monthly_sales_by_product ORDER BY polish_store.monthly_sales_by_product.Product_Id_Num;
@@ -31,7 +36,6 @@ HAVING UniqueProductNames > 1;
 
 -- CREATE TEMP TABLE. 
 -- I SELECTED THE BASIC COLUMNS, I WILL NOT ADD COLUMNS SUCH AS AVG PURCHASE AND SALE PRICE AS THEY CHANGE CONSTANTLY.
-
 SELECT 
 	ps.Product_Id_Num,
     MAX(ps.Product_name) AS ProductName,
@@ -58,9 +62,25 @@ ORDER BY
 	ps.Product_Id_Num;
 
 
-
 -- VERIFY NEW TABLE CREATION
 SELECT * FROM polish_store.products;
+
+
+-- SET THE PRIMARY KEY FOR TABLE PRODUCTS
+ALTER TABLE polish_store.products ADD PRIMARY KEY(Product_Id_Num);
+
+
+-- CONFIRM PRIMARY KEY WAS SET
+DESCRIBE polish_store.products;
+
+
+-- SET FOREIGN KEY REFERENCING RELATED PRODUCT
+ALTER TABLE polish_store.monthly_sales_by_product
+ADD FOREIGN KEY(Product_Id_Num) REFERENCES products(Product_Id_Num);
+
+
+-- CONFIRM FOREIGN KEY WAS SET
+DESCRIBE polish_store.monthly_sales_by_product;
 
 
 
